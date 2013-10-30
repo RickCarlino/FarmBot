986536a62c47b125250067096d003ef68d5a8e7f
rick carlino <rick.carlino@gmail.com>
2013-10-29 21:15:32 -0700

    Initial commit for Farmbot Backend Web Services

---
+++ backend/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Backend::Application.config.secret_key_base = 'ddb79e328d45a0edfc9f9ed1c1967c42585e4eae307d0664794760b8c866023824a7113fbb4551c5433e5b5823d9be646d4d9b7fa9a1fafaf4cced8ea6d5de08'
